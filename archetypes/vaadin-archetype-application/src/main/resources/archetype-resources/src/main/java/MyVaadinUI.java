e8605dff7866d9b62d120e32ca007945efbc6459
Henri Sara <henri.sara@itmill.com>
2012-09-06 10:58:47 +0000

    #7456 Initial version of a Vaadin 7 application archetype
    git-svn-id: http://dev.vaadin.com/svn/integration/maven@24337 de684a11-831f-0410-ab27-cb86445eb682

---
+++ archetypes/vaadin-archetype-application/src/main/resources/archetype-resources/src/main/java/MyVaadinUI.java
@@ -0,0 +1,30 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import com.vaadin.server.WrappedRequest;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+
+/**
+ * The Application's "main" class
+ */
+@SuppressWarnings("serial")
+public class MyVaadinUI extends UI
+{
+
+    @Override
+    protected void init(WrappedRequest request) {
+        Button button = new Button("Click Me");
+        button.addListener(new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                event.getButton().getUI().addComponent(new Label("Thank you for clicking"));
+            }
+        });
+        addComponent(button);
+    }
+
+}
