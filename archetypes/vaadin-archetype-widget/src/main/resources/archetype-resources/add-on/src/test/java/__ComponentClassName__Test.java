ced15c64c3a52f003abbddf1727f2da53a15b162
Henri Sara <hesara@vaadin.com>
2013-04-17 10:00:24 +0300

    Add a unit test and surefire plugin for widget archetype
    Change-Id: Ia704ddc0d3cbd9bbabaeb4042395f1745894d35b

---
+++ archetypes/vaadin-archetype-widget/src/main/resources/archetype-resources/add-on/src/test/java/__ComponentClassName__Test.java
@@ -0,0 +1,13 @@
+package ${package};
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+// JUnit tests here
+public class ${ComponentClassName}Test {
+
+	@Test
+	public void thisAlwaysPasses() {
+		Assert.assertEquals(true, true);
+	}
+}
