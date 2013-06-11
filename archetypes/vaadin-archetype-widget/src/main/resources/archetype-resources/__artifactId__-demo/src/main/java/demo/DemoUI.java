93d6fe3bb9076cd6a608f9be9cbac522d67b48c4
Henri Sara <hesara@vaadin.com>
2013-06-11 13:26:21 +0300

    Use Servlet 3.0 in widget archetype, no web.xml (#11764)
    This version is only compatible with Vaadin 7.1 and later (demo module).
    The template is similar to the one used by the Eclipse plug-in.
    
    Change-Id: I3334c355c812cb54976925780fc573079269782e

--- archetypes/vaadin-archetype-widget/src/main/resources/archetype-resources/__artifactId__-demo/src/main/java/demo/DemoUI.java
+++ archetypes/vaadin-archetype-widget/src/main/resources/archetype-resources/__artifactId__-demo/src/main/java/demo/DemoUI.java
@@ -2,9 +2,13 @@ package ${package}.demo;
 
 import ${package}.${ComponentClassName};
 
+import javax.servlet.annotation.WebServlet;
+
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
+import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -15,14 +19,19 @@ import com.vaadin.ui.VerticalLayout;
 public class DemoUI extends UI
 {
 
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class, widgetset = "${package}.demo.DemoWidgetSet")
+    public static class Servlet extends VaadinServlet {
+    }
+
     @Override
     protected void init(VaadinRequest request) {
-    	
-    	// Initialize our new UI component
-    	final ${ComponentClassName} ${artifactId} = new ${ComponentClassName}();
-            	
-    	// Show it in the middle of the screen
-    	final VerticalLayout layout = new VerticalLayout();
+
+        // Initialize our new UI component
+        final ${ComponentClassName} ${artifactId} = new ${ComponentClassName}();
+
+        // Show it in the middle of the screen
+        final VerticalLayout layout = new VerticalLayout();
         layout.setStyleName("demoContentLayout");
         layout.setSizeFull();
         layout.addComponent(${artifactId});
