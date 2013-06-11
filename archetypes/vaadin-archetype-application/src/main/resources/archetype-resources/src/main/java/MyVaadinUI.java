0e0cc1f9278d862dc355afdda51e4b69abf60120
Henri Sara <hesara@vaadin.com>
2013-06-11 12:19:01 +0300

    Use Servlet 3.0 in application archetype, no web.xml (#11764)
    This version is only compatible with Vaadin 7.1 and later.
    The template is close to the one used by the Eclipse plug-in, but also
    contains a widgetset.
    
    Change-Id: Ia0b9cf8c0ba9962b72ad09a69b7300bb6e839910

--- archetypes/vaadin-archetype-application/src/main/resources/archetype-resources/src/main/java/MyVaadinUI.java
+++ archetypes/vaadin-archetype-application/src/main/resources/archetype-resources/src/main/java/MyVaadinUI.java
@@ -3,19 +3,26 @@
 #set( $symbol_escape = '\' )
 package ${package};
 
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-/**
- * The Application's "main" class
- */
 @SuppressWarnings("serial")
 public class MyVaadinUI extends UI
 {
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "${package}.AppWidgetSet")
+    public static class Servlet extends VaadinServlet {
+    }
 
     @Override
     protected void init(VaadinRequest request) {
