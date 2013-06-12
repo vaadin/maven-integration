066e5e8583cd84de0c9a0bf42c583c364a141390
Henri Sara <hesara@vaadin.com>
2013-06-12 15:41:38 +0300

    Create theme in application archetype (#11771)
    Change-Id: Ic1fdff6a3d4eee7c185bb5f439909383b756f9a1

--- archetypes/vaadin-archetype-application/src/main/resources/archetype-resources/src/main/java/MyVaadinUI.java
+++ archetypes/vaadin-archetype-application/src/main/resources/archetype-resources/src/main/java/MyVaadinUI.java
@@ -15,6 +15,7 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+@Theme("${theme}")
 @SuppressWarnings("serial")
 public class MyVaadinUI extends UI
 {
