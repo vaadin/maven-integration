186a67a7e9a6c097d76e3be221b78a6c8d68a089
Teemu Suo-Anttila <teemusa@vaadin.com>
2016-03-07 16:03:16 +0200

    Add class level javadoc to generate UI in application archetype
    Change-Id: I63942d3a60d7b74f0ba83479610c2c6f765b411b

--- archetypes/vaadin-archetype-application/src/main/resources/archetype-resources/src/main/java/__uiName__.java
+++ archetypes/vaadin-archetype-application/src/main/resources/archetype-resources/src/main/java/__uiName__.java
@@ -18,7 +18,11 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 /**
- *
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add component to the user interface and initialize non-component functionality.
  */
 @Theme("${themeName}")
 @Widgetset("${package}.${widgetsetName}")
